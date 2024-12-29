7145336db7ea7272082de796506b1f12d81d9bba
Satyan2309 <144508837+Satyan2309@users.noreply.github.com>
2024-12-29 17:27:28 +0530

    3rd commit mongodb and crated owner

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,8 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const config = require("config");
 
-mongoose.connect("mongodb://127.0.0.1:27017/project")
+const dbgr = require('debug')('development:mongoose');
+
+
+mongoose
+.connect(`${config.get("MONGODB_URI")}/project`)
 .then(function(){
-    console.log('connected');
+    dbgr('connected');
 })
 .catch(function(err){
     console.log(err);
--- models/user-model.js
+++ models/user-model.js
@@ -14,7 +14,7 @@ const userSchema = mongoose.Schema({
         default:[]
     },
     isadmin :Boolean,
-    orders :{
+    orders :{     
         type: Array,
         default:[]
     },
